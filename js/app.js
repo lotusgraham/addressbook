6c5fb18b58ea9588747e3d1af6d162ce81708948
Bhaumik Patel <bhaumik@thinkful.com>
2016-04-15 15:29:47 -0400

    Adds ability to store contact in contact list

--- js/app.js
+++ js/app.js
@@ -2,22 +2,6 @@
 // upon click on add, input should save into new contact
 // new contact added to ul/list
 // upon click on specific contact, contact view is displayed
-//
-// var contact = {
-//     fullName: "Graham Whitley",
-//     firstName: "Graham",
-//     lastName: "Whitley",
-//     phoneNumber: "252-286-8899",
-//     street: "209 e. knox st.",
-//     city: "Durham",
-//     state: "NC"
-// };
-//  var newContact =   Object.create(contact);
-//
-//  newContact.fullName = $('.firstname').text('');
-
-//create an object named Contact
-//store first name last name, number, street, city, and state in Contact
 
 
 function Contact(first, last, phone, mystreet, mycity, mystate) {
@@ -30,13 +14,10 @@ function Contact(first, last, phone, mystreet, mycity, mystate) {
     this.state = mystate;
 }
  $('#addstuff').click(function(){
-     console.log(newContact());
-
+ 	var newContact = new Contact($('.firstName').val(),  $('.lastName').val(), $('.phoneNumber').val() );
+ 	$('.items').append('<li><a href="x.com">' + $('.firstName').val() + '</a></li>');
+ 	console.log(newContact);    
+    arrayOfContacts.push(newContact);
  });
 
-var testing = new Contact( "Graham W",  "Graham",  "Whitley" );
-
-var newContact = new Contact($('.firstName').val(),  $('.lastName').val(), $('phoneNumber').val() );
-
-//
-// var question1 = new Question("Name?", ["Will", "Graham"], "Graham");
+var arrayOfContacts = [];
