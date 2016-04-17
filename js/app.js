62d3bed70a6dd820e8fcbc8d29f866465082b8cd
lotusgraham <gwhitley99@gmail.com>
2016-04-17 13:14:35 -0400

    beginning to build function for specific contact to show when clicked

--- js/app.js
+++ js/app.js
@@ -13,11 +13,31 @@ function Contact(first, last, phone, mystreet, mycity, mystate) {
     this.city = mycity;
     this.state = mystate;
 }
+
+var indexOfContact = 0;
+
  $('#addstuff').click(function(){
- 	var newContact = new Contact($('.firstName').val(),  $('.lastName').val(), $('.phoneNumber').val() );
- 	$('.items').append('<li><a href="x.com">' + $('.firstName').val() + '</a></li>');
- 	console.log(newContact);    
+ 	var newContact = new Contact($('.firstName').val(),  $('.lastName').val(), $('.phoneNumber').val(), $('.street').val(), $('.city').val(), $('.state').val() );
+ 	$('.items').append('<li class="specificItem" data=' + indexOfContact + '><a>' + $('.firstName').val() + '</a></li>');
+    indexOfContact++;
+ 	console.log(newContact);
     arrayOfContacts.push(newContact);
  });
 
 var arrayOfContacts = [];
+
+// $('.items').click(function(){
+//     $('.displayedFirstName').append(arrayOfContacts[0].firstName);
+//     $('.displayedLastName').append(arrayOfContacts[0].lastName);
+// });
+
+
+$('.items').on("click", ".specificItem", function(){
+    console.log($(this));
+    $('.displayedFirstName').append(arrayOfContacts[0].firstName);
+    $('.displayedLastName').append(arrayOfContacts[0].lastName);
+});
+
+// get the data attribute from the li that got clicked (=firstname)
+// look thru contacts for a firstname match
+// once you get the correct contact, load their properties into the html
